0dacccadc14e7820d2700f8166aff62c23f9f5d0
Newton@Newton-PC-2_WSL <littleNewton6@gmail.com>
2019-12-18 12:36:13 +0800

    Rearranged the File Folders

---
+++ powershell/os-update.ps1
@@ -0,0 +1,15 @@
+function Update-Packages {
+    # update pip
+    Write-Output("Step 1: update pip")
+    $a = pip list --outdated
+    $num_package = $a.Length - 2
+    for ($i = 0; $i -lt $num_package; $i++) {
+        $tmp = ($a[2 + $i].Split(" "))[0]
+        pip install -U $tmp
+    }
+
+    # update TeX Live 2019
+    Write-Output("Step 2: update TeX Live 2019")
+    tlmgr update --self
+    tlmgr update --all
+}
